283e32714b0208bb6b8bbbf45c9199a952727662
Laith-Grira <lgrira12@gmail.com>
2021-07-10 16:55:13 -0400

    adjusting the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import './Navbar.css';
+import Logo from '../../assets/logo/ottawa-united-logo.gif';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
         <div className="nav-container">
             <nav className="navbar">
+                <img src={Logo} alt="team logo" /> <h3>OTTAWA UNITED F.C.</h3>
                 <div className="links">
                     <Link to="/">Home</Link>
                     <Link to="/team">Team</Link>
