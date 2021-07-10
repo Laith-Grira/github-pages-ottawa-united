12c3378c213e0f3cb78fe3b7a4c49989eaeaacd0
Laith-Grira <lgrira12@gmail.com>
2021-07-10 12:52:44 -0400

    ading navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+        <div className="nav-container">
+            <nav className="navbar">
+                <div className="links">
+                    <Link to="/">Home</Link>
+                    <Link to="/team">Team</Link>
+                    <Link to="/fixtures">Fixtures</Link>
+                    <Link to="/tickets">Tickets</Link>
+                    <Link to="/stats">Stats</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
