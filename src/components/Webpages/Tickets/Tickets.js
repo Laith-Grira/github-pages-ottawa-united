eaef70c47c6039d5df665f2f274ccd065a221258
Laith-Grira <lgrira12@gmail.com>
2021-07-10 15:44:26 -0400

    some clean up

--- src/components/Webpages/Tickets/Tickets.js
+++ src/components/Webpages/Tickets/Tickets.js
@@ -4,13 +4,6 @@ const Tickets = () => {
     return ( 
         <div className="main-tickets">
             <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
-            <h1>Tickets</h1>
         </div>
      );
 }
