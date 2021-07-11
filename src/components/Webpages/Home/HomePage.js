4284079c2594fcc35e0dbfe48c37ec056fe44081
Laith-Grira <lgrira12@gmail.com>
2021-07-11 15:38:23 -0400

    finishing the news feed

--- src/components/Webpages/Home/HomePage.js
+++ src/components/Webpages/Home/HomePage.js
@@ -2,6 +2,9 @@ import './HomePage.css';
 import { Link } from 'react-router-dom';
 import Logo from '../../../assets/logo/OttawaUnited.png';
 import teamImage from '../../../assets/pictures/teamImage.jpg';
+import kakaManager from '../../../assets/pictures/kakaManager.jpg';
+import xhaka from '../../../assets/pictures/xhaka.jpg';
+import stadium from '../../../assets/pictures/stadium.jpg';
 
 const HomePage = () => {
     return(
@@ -20,9 +23,24 @@ const HomePage = () => {
             </Link>
             <h1>News Of Our Team</h1>
             <div className="news-feed">
-                <div className="news1"></div>
-                <div className="news2"></div>
-                <div className="news3"></div>
+                <div className="news1">
+                    <a href="https://acmilanspot.com/kaka-i-would-love-to-start-my-career-as-manager-in-milan/" target="_blank">
+                        <img src={kakaManager} alt="kaka" />
+                        <p><b>Kaka is looking forward to be our new team manager for this season</b></p>
+                    </a>
+                </div>
+                <div className="news2">
+                    <a href="https://talksport.com/football/904786/switzerland-granit-xhaka-england-euro-2020-semi-finals/" target="_blank">
+                        <img src={xhaka} alt="xhaka" />
+                        <p><b>Our player Xhaka lost in the Euro 2020 against England and left the tournement</b></p>
+                    </a>
+                </div>
+                <div className="news3">
+                    <a href="https://the18.com/soccer-entertainment/smallest-soccer-stadiums-in-the-top-flights" target="_blank">
+                        <img src={stadium} alt="xhaka" />
+                        <p><b>Our Team managment is looking forward to expend the stadium capacity by 10,000 seats</b></p>
+                    </a>
+                </div>
             </div>
             <h1>Watch Our Team In Action</h1>
             <div className="videos">
