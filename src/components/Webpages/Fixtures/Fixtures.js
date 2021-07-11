9907085bf4ea60454876b9aa571e8c122101fa31
Laith-Grira <lgrira12@gmail.com>
2021-07-11 19:01:26 -0400

    adjusting the boxes

--- src/components/Webpages/Fixtures/Fixtures.js
+++ src/components/Webpages/Fixtures/Fixtures.js
@@ -3,7 +3,32 @@ import './Fixtures.css';
 const Fixtures = () => {
     return ( 
         <div className="main-fixtures">
-            <h1>Fixtures</h1>
+            <div className="premier-league">
+                <h1>Canada Premier League</h1>
+                <div className="match">
+                    <div className="teams"></div>
+                    <div className="match-info"></div>
+                    <div className="stadium"></div>
+                </div>
+                <div className="match">
+                    <div className="teams"></div>
+                    <div className="match-info"></div>
+                    <div className="stadium"></div>
+                </div>
+                <div className="match">
+                    <div className="teams"></div>
+                    <div className="match-info"></div>
+                    <div className="stadium"></div>
+                </div>
+            </div>
+            <div className="league-cup">
+                <h1>League Cup</h1>
+                <div className="match">
+                    <div className="teams"></div>
+                    <div className="match-info"></div>
+                    <div className="stadium"></div>
+                </div>
+            </div>
         </div>
      );
 }
