08f5e3ab83ce00bc7538dbcf9e33d437f1dbdaeb
Laith-Grira <lgrira12@gmail.com>
2021-07-11 19:39:43 -0400

    finishing adding the new teams

--- src/components/Webpages/Fixtures/Fixtures.js
+++ src/components/Webpages/Fixtures/Fixtures.js
@@ -3,6 +3,9 @@ import './Fixtures.css';
 // Teams logos
 import OUFC from '../../../assets/teams/ottawa-united-logo.gif';
 import Halifax from '../../../assets/teams/halifax.png';
+import Edmonton from '../../../assets/teams/edmonton.png';
+import York from '../../../assets/teams/york.png';
+import Forge from '../../../assets/teams/forge.png';
 import Away from '../../../assets/icons/away.png';
 import Home from '../../../assets/icons/stadium.png';
 
@@ -28,14 +31,14 @@ const Fixtures = () => {
                 </div>
                 <div className="match">
                     <div className="teams">
-                        <img src={Halifax} alt="Halifax" className="team-pic" />
+                        <img src={York} alt="York" className="team-pic" />
                         <p className="vs-tag">VS</p>
                         <img src={OUFC} alt="Ottawa United" className="team-pic" />
                     </div>
                     <div className="match-info">
-                        <p>Ottawa United vs Halifax <br />
-                           Date: Auguest 27th, 2021 <br />
-                           MatchDay: 13</p>
+                        <p>York United vs Ottawa United <br />
+                           Date: Auguest 31th, 2021 <br />
+                           MatchDay: 14</p>
                     </div>
                     <div className="stadium">
                         <img src={Away} alt="away" className="team-pic" />
@@ -43,14 +46,14 @@ const Fixtures = () => {
                 </div>
                 <div className="match">
                     <div className="teams">
-                        <img src={Halifax} alt="Halifax" className="team-pic" />
+                        <img src={Edmonton} alt="Edmonton" className="team-pic" />
                         <p className="vs-tag">VS</p>
                         <img src={OUFC} alt="Ottawa United" className="team-pic" />
                     </div>
                     <div className="match-info">
-                        <p>Ottawa United vs Halifax <br />
-                           Date: Auguest 27th, 2021 <br />
-                           MatchDay: 13</p>
+                        <p>FC Edmonton vs Ottawa United <br />
+                           Date: September 7th, 2021 <br />
+                           MatchDay: 15</p>
                     </div>
                     <div className="stadium">
                         <img src={Away} alt="away" className="team-pic" />
@@ -63,12 +66,12 @@ const Fixtures = () => {
                     <div className="teams">
                         <img src={OUFC} alt="Ottawa United" className="team-pic" />
                         <p className="vs-tag">VS</p>
-                        <img src={Halifax} alt="Halifax" className="team-pic" />
+                        <img src={Forge} alt="Forge" className="team-pic" />
                     </div>
                     <div className="match-info">
-                        <p>Ottawa United vs Halifax <br />
-                           Date: Auguest 27th, 2021 <br />
-                           MatchDay: 13</p>
+                        <p>Ottawa United vs FC Forge <br />
+                           Date: September 21st, 2021 <br />
+                           Quarter Final</p>
                     </div>
                     <div className="stadium">
                         <img src={Home} alt="home" className="team-pic" />
