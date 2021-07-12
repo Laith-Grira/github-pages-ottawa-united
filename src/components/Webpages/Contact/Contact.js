dd6cb18749c921a86cea7769bf763c9f11a29c2b
Laith-Grira <lgrira12@gmail.com>
2021-07-12 01:47:39 -0400

    adding event handeler for contact page

--- src/components/Webpages/Contact/Contact.js
+++ src/components/Webpages/Contact/Contact.js
@@ -1,6 +1,18 @@
 import './Contact.css';
+import { useEffect, useState } from "react";
+
 
 const Contact = () => {
+
+    // Email variables
+    const [name, setName] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [emailMsg, setEmailMsg] = useState([]);
+
+    // Tweet variables
+    const [twitterID, setTwitterID] = useState([]);
+    const [twitterMsg, setTwitterMsg] = useState([]);
+
     return ( 
         <div className="main-contact">
             <h1>Contact page</h1>
@@ -11,10 +23,37 @@ const Contact = () => {
                     information and send us an email defining your enquiry. (We respond within 3 business days)
                 </p>
                 <form action="#" method="post">
-                    <input type="text" placeholder="Full Name" className="mb-2"/><br />
-                    <input type="text" placeholder="Email Address" className="mb-2"/><br />
-                    <textarea name="msg" id="msg" cols="30" rows="10" placeholder="Email message..."></textarea><br/>
-                    <button type="button" class="btn btn-secondary send-email"><b>Send Email</b></button>
+                    <input type="text" 
+                            placeholder="Full Name" 
+                            className="mb-2"
+                            onChange={ e => setName(e.target.value)}/>
+                    <br />
+                    <input type="text" 
+                            placeholder="Email Address" 
+                            className="mb-2"
+                            onChange={ e => setEmail(e.target.value)}/>
+                    <br />
+                    <textarea name="msg" 
+                                id="msg" 
+                                cols="30" 
+                                rows="10" 
+                                placeholder="Email message..."
+                                onChange={ e => setEmailMsg(e.target.value)}></textarea>
+                                <br/>
+                    <button type="button" 
+                            className="btn btn-secondary send-email"
+                            onClick={ () => {
+                                if (name == '') {
+                                    alert("Please insert your Full Name");
+                                } else if (email == ''){
+                                    alert("Please insert your email");
+                                } else if (emailMsg == '') {
+                                    alert('Please enter an email message');
+                                } else {
+                                    alert("Email is sent successfully!");
+                                }
+                            }}
+                            ><b>Send Email</b></button>
                 </form>
             </div>
             <div className="quick-contact">
@@ -23,9 +62,32 @@ const Contact = () => {
                     to our team. (Please note that every tweet is being monitred by our admin).
                 </p>
                 <form action="#" method="post">
-                    <input type="text" placeholder="@example" title="Insert your twitter id here." className="mb-2"/><br />
-                    <textarea name="msg" id="msg" cols="30" rows="5" placeholder="Message..."></textarea><br/>
-                    <button type="button" className="btn btn-secondary send-email"><b>Tweet</b></button>
+                    <input type="text" 
+                            placeholder="@example" 
+                            title="Insert your twitter id here." 
+                            className="mb-2"
+                            onChange={ e => setTwitterID(e.target.value)}/>
+                            <br />
+                    <textarea name="msg" 
+                                id="msg" 
+                                cols="30" 
+                                rows="5" 
+                                placeholder="Message..."
+                                onChange={ e => setTwitterMsg(e.target.value)}
+                                ></textarea><br/>
+                    <button type="button" 
+                            className="btn btn-secondary send-email"
+                            onClick={() => {
+                                if (twitterID == '') {
+                                    alert("Please insert your Twitter ID");
+                                } else if (twitterID[0] !="@"){
+                                    alert("Please make sure you add the '@' at the begining");
+                                } else if (twitterMsg == '') {
+                                    alert('Please enter a message in the text area');
+                                } else {
+                                    alert("Tweet is sent successfully!");
+                                }
+                            }}><b>Tweet</b></button>
                 </form>
             </div>
         </div>
